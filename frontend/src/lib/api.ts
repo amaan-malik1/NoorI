7dd15c21963182ebe109876e6351d9ad992c62c4
Amaan Malik <amikm1077@gmail.com>
2026-06-21 18:37:54 +0530

    fix: API and packages

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,8 +1,7 @@
 import axios from 'axios'
 import { useAuthStore } from '@/store/auth.store'
 
-const API_URL = import.meta.env.VITE_API_URL as string
-console.log("API backend: ",import.meta.env.VITE_API_URL)
+const API_URL = import.meta.env.VITE_API_URL as string;
 
 export const api = axios.create({
   baseURL: API_URL,
